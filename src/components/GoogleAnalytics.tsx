48c4302e1298ceec59ddb0437e67ab8b61a9cd3f
Fernando Rhenals <rhenalsf@gmail.com>
2025-08-26 20:47:52 -0500

    Ajustes Google Analytics

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,7 +2,7 @@
 
 import Script from 'next/script';
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-ZJN4B4BQ20';
 
 export default function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) {
